5691f963cfc986eb0a935f81054d386209cfe931
liviaruegger <analiviaruegger@gmail.com>
2023-10-30 11:35:51 -0300

    feat: mockando todas as funcionalidades

--- ep2.sh
+++ ep2.sh
@@ -11,9 +11,6 @@ CHAT_ID="1746769700"
 
 URL="https://api.telegram.org/bot${TOKEN}/sendMessage?chat_id=${CHAT_ID}"
 
-# Testar o envio de mensagem:
-# curl -s --data "text=ola, mundo!" $URL 1>/dev/null
-
 
 # VARIÁVEIS GLOBAIS ------------------------------------------------------------
 
@@ -26,19 +23,6 @@ encerrar=0
 
 function msg_telegram {
     curl -s --data "text=$1" $URL 1>/dev/null    
-}
-
-function quit {
-    # TODO - se for cliente, faz logout caso o user esteja logado
-    # if ["$MODO" = "cliente"]; then
-    #     if ["$logado" = "1"]; then
-    #         # logout
-    #     fi
-    # fi
-
-    msg_telegram "encerrando ${MODO}"
-
-    encerrar=1
 }
 
 
@@ -57,10 +41,107 @@ echo "executando modo: ${MODO}"
 msg_telegram "executando modo: ${MODO}"
 
 
+# FUNÇÕES GERAIS ---------------------------------------------------------------
+
+function list {
+    # TODO - implementar
+    echo "mock: executando list"
+}
+
+function quit {
+    # TODO - se for cliente, faz logout caso o user esteja logado
+    # if ["$MODO" = "cliente"]; then
+    #     if ["$logado" = "1"]; then
+    #         # logout
+    #     fi
+    # fi
+
+    msg_telegram "encerrando ${MODO}"
+
+    encerrar=1
+}
+
+
 # IMPLEMENTAÇÃO DO SERVIDOR ----------------------------------------------------
+
+# Comandos disponíveis: list, time, reset, quit
+
+function time {
+    # TODO - implementar
+    echo "mock: executando time"
+}
+
+function reset {
+    # TODO - implementar
+    echo "mock: executando reset"
+}
 
 
 # IMPLEMENTAÇÃO DO CLIENTE -----------------------------------------------------
+
+# Comandos disponíveis: create, passwd, login, logout, list, msg, quit
+
+function create { # TEM MAIS DOIS ARGUMENTOS
+    usuario=${arg_1}
+    senha=${arg_2}
+    
+    # TODO - implementar
+
+    # MOCK
+    echo "mock: executando create"
+    echo "usuario: ${usuario}"
+    echo "senha: ${senha}"
+}
+
+function passwd {
+    usuario=${arg_1}
+    senha_antiga=${arg_2}
+    senha_nova=${arg_3}
+
+    # TODO - implementar
+
+    # MOCK
+    echo "mock: executando passwd"
+    echo "usuario: ${usuario}"
+    echo "senha_antiga: ${senha_antiga}"
+    echo "senha_nova: ${senha_nova}"
+}
+
+function login {
+    usuario=${arg_1}
+    senha=${arg_2}
+
+    # TODO - questão: a senha pode conter espaço?
+
+    # TODO - implementar
+
+    # MOCK
+    echo "mock: executando login"
+    echo "usuario: ${usuario}"
+    echo "senha: ${senha}"
+}
+
+function logout {
+    # TODO - implementar
+    echo "mock: executando logout"
+}
+
+function msg {
+    usuario=${arg_1}
+    mensagem="${arg_2}"
+
+    # Caso a mensagem contenha espaço, é preciso juntas as partes
+    if ! [ "$arg_3" = "" ]; then
+        mensagem="${arg_2} ${arg_3}"
+    fi
+
+    # TODO - implementar
+
+    # MOCK
+    echo "mock: executando msg"
+    echo "usuario: ${usuario}"
+    echo "mensagem: ${mensagem}"
+}
 
 
 # RECEBER E EXECUTAR COMANDOS --------------------------------------------------
@@ -77,9 +158,7 @@ while [ $encerrar = 0 ]; do
     echo -n "${MODO}> "
     
     IFS=" "
-    read COMANDO
-
-    # TODO - Como ler os argumentos do comando?
+    read COMANDO arg_1 arg_2 arg_3
 
     # Executa apenas se COMANDO estiver em COMANDOS_VALIDOS
     if [[ " ${COMANDOS_VALIDOS[@]} " =~ " ${COMANDO} " ]]; then
